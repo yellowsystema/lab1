eb09a74ef2b6d9c8cdbd911fcf81d889fbefb1b2
Katharine Sato <87663402+yellowsystema@users.noreply.github.com>
2024-10-01 18:29:08 +0000

    dd

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main() {
   char hello[] = "Hello! Wassuppp"; 
-  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello2[] = { 52, 35, 108, 108, 111, 33, 0 };
 
   puts(hello);
   puts(hello2);
